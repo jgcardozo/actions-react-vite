aa0d8328e7e660d92984dd09094b54012c8e098e
juancardozo-idea <juan.cardozo@ideaware.co>
2022-02-23 14:56:50 -0500

    fix base:param on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://jgcardozo.github.io/',
+  base: 'https://jgcardozo.github.io/actions-react-vite/',
   plugins: [react()]
 })
